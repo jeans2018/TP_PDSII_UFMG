93596d0405abe9f12d236a5cf1474cedeaf53a6d
EmersonAS <37188272+EmersonAS@users.noreply.github.com>
2018-11-18 17:19:12 -0200

    Leitura de arquivos e processamento
    Leitura de arquivos:
    1-Implementação da função LeAqruivo, reponsável por ler o arquivo de entrada, processá-lo e inseri-lo em um map. Utiliza processamento de arquivos de entrada, criação de pares de duas strings e a função membro inserir da classe IndiceInvertido.
    
    2-Implementação da função AjustaString, responsável por remover todos os caracteres especiais que não forem letras ou números das palavras a serem lidas dos arquivos e retornar essa palavra ajustada. Utiliza as funções isalpha(), para determinar se um caracter é uma letra, isdigit(), para determinar se o caracter é um dígito de 0 a 9, além da função tolower(), para transformar o caracter de maiúsculo para minúsculo.

--- include/IndiceInvertido.h
+++ include/IndiceInvertido.h
@@ -11,27 +11,36 @@ using std::map;
 using std::set;
 using std::pair;
 
+typedef map< string,set<string>, std::less<string> > StrMap;
+
 class IndiceInvertido
 {
     public:
         IndiceInvertido();
+	
         ~IndiceInvertido();
+	
         //Insere um item no dicionário de forma ordenada, caso a chave já pertença
         //ao dicionário, a lista associada à ele é atualizada
         void inserir(const pair<string,string>& item);
+	
         //Remove um item do dicionário, se ele lá estiver
         void remover(const string chave);
+	
         //Verifica se o dicionário está vazio
         bool vazio()const;
+	
         //Retorna a quantidade de itens no dicionário
         int tamanho()const;
+	
         //Verifica se a string chave pertence ao índice
         bool pertence(const string chave);
+	
         //Retorna o set indexado por chave
         const set<string>& buscar(const string chave);
 
     private:
-       map< string,set<string> > indice_;
+       StrMap indice_;
 };
 
 #endif // INDICEINVERTIDO_H
