18ca7c0f70ceb98e81eb70200112941b13df64b7
EmersonAS <37188272+EmersonAS@users.noreply.github.com>
2018-11-19 19:26:52 -0200

    Testes
    Testes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-//#include <string>
-//#include <set>
+#include <string>
+#include <set>
+#include <iterator>
 
 #include "IndiceInvertido.h"
 #include "Useful.h"
@@ -15,6 +16,26 @@ int main()
     LeArquivo("macbeth.txt", indice);
     LeArquivo("othello.txt", indice);
     LeArquivo("romeoandjuliet.txt", indice);
+
+    string chaveDeBusca;
+    set<string> resultBusca;
+    std::ostream_iterator<string> output(cout, " ");
+
+    cout << "\nDigite uma palavra: ";
+    cin >> chaveDeBusca;
+
+    resultBusca = indice.buscar(chaveDeBusca);
+
+        // Imprime a lista de documentos retornada na tela
+    if(!resultBusca.empty())
+    {
+        cout << endl;
+        std::copy(resultBusca.begin(),resultBusca.end(), output);
+        cout << endl;
+    }
+
+    // Imprime a lista de documentos retornada na tela
+    //for()
 
     return 0;
 }
